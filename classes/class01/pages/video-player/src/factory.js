ea387cef2d8b40c8b7d7d440eb38f67adedf7ade
João Pedro <jpcollege0101@gmail.com>
2023-02-28 17:39:53 -0300

    First commit

---
+++ classes/class01/pages/video-player/src/factory.js
@@ -0,0 +1,17 @@
+import controller from "./controller.js"
+
+const [rootPath] = window.location.href.split('/pages/')
+const factory = {
+  async initialize() {
+    return Controller.initialize({
+     /* view: new CardsView(),
+      service: new CardsService({ 
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+      })
+     */ 
+    })
+ }
+}
+ 
+export default factory
